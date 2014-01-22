0002fb528a5e08968f877d467be2656e3602c522
Dave Humphrey <uespnet@gmail.com>
2014-01-22 14:24:36 -0500

    revert to minimal tile loads when dragging

--- gamemap.js
+++ gamemap.js
@@ -416,9 +416,7 @@ uesp.gamemap.Map.prototype.shiftMapTiles = function(deltaX, deltaY)
 	this.startTileX += deltaX;
 	this.startTileY += deltaY;
 	
-	//console.debug("shiftMapTiles(" + deltaX + ", " + deltaY + "): " + this.startTileX + ", " + this.startTileY);
 	this.loadMapTilesRowCol(targetXIndex + (this.mapOptions.tileCountX - 1) * deltaX, targetYIndex + (this.mapOptions.tileCountY - 1) * deltaY);
-	//this.loadMapTiles();
 }
 
 
