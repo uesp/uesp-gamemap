002cf5dd546ad393dd7a12016415ba827ce980b2
Dave Humphrey <uespnet@gmail.com>
2014-01-22 18:15:01 -0500

    add basic map state and worlds

---
+++ mapstate.js
@@ -0,0 +1,13 @@
+/*
+ * mapstate.js -- Created by Dave Humphrey (dave@uesp.net) on 22 Jan 2014
+ * 		Released under the GPL v2
+ *
+ */
+
+
+uesp.gamemap.MapState = function()
+{
+	this.zoomLevel = 10;
+	this.gamePos = new uesp.gamemap.Position(0, 0);
+	this.worldName = "__default";
+}
---
+++ world.js
@@ -0,0 +1,15 @@
+/*
+ * world.js -- Created by Dave Humphrey (dave@uesp.net) on 22 Jan 2014
+ * 		Released under the GPL v2
+ *
+ */
+
+
+uesp.gamemap.World = function(worldName, mapOptions)
+{
+	this.worldName = worldName;
+	this.mapOptions = new uesp.gamemap.MapOptions(mapOptions);
+	this.mapState = new uesp.gamemap.MapState();
+}
+
+
