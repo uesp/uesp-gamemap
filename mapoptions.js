1d0f3a1138a19e8d106ec075508138476d3caa67
Dave Humphrey <uespnet@gmail.com>
2014-01-22 09:22:39 -0500

    add missing mapoptions

---
+++ mapoptions.js
@@ -0,0 +1,36 @@
+/*
+ * mapoptions.js -- Created by Dave Humphrey (dave@uesp.net) on 22 Jan 2014
+ * 		Released under the GPL v2
+ *
+ */
+
+uesp.gamemap.MapOptions = function(mapOptions)
+{
+	this.getMapTileFunction = uesp.gamemap.defaultGetMapTile;
+	
+	this.minZoomLevel = 9;
+	this.maxZoomLevel = 17;
+	
+	this.tileCountX = 7;
+	this.tileCountY = 5;
+	this.tileSize   = 256;
+	
+	this.gamePosX1 = 0;
+	this.gamePosX2 = 0;
+	this.gamePosY1 = 10000;
+	this.gamePosY2 = 10000;
+	
+	this.mapContainer = "#gmMap";
+	
+	this.debug = true;
+	
+	//console.debug("Before: " + this.gamePosX1);
+	
+	if (!uesp.gamemap.isNullorUndefined(mapOptions))
+	{
+		uesp.gamemap.mergeObjects(this, mapOptions);
+	}
+	
+	//console.debug("Before: " + this.gamePosX1);
+	//console.debug("Input: " + mapOptions.gamePosX1);
+}
