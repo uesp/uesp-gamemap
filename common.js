c08c1088dce92e189f54448ea304321549dcffb6
Dave Humphrey <uespnet@gmail.com>
2014-01-22 20:50:49 -0500

    change console calls to methods

--- common.js
+++ common.js
@@ -9,6 +9,16 @@
 var uesp = uesp || {};
 uesp.gamemap = uesp.gamemap || {};
 
+uesp.DEFAULT_DEBUG = true;
+
+uesp.LOG_LEVEL_NONE     = 6;
+uesp.LOG_LEVEL_CRITICAL = 5;
+uesp.LOG_LEVEL_ERROR    = 4;
+uesp.LOG_LEVEL_WARNING  = 3;
+uesp.LOG_LEVEL_INFO     = 2;
+uesp.LOG_LEVEL_ALL      = 1;
+uesp.DEFAULT_LOG_LEVEL = uesp.LOG_LEVEL_ERROR;
+
 
 	// Simple position/point class
 uesp.gamemap.Position = function (x, y)
@@ -38,3 +48,100 @@ uesp.gamemap.mergeObjects = function (obj1, obj2)
 	for (var attrname in obj2) { obj1[attrname] = obj2[attrname]; }
 	return obj1;
 }
+
+
+uesp.priv_logDebug = function () {
+	var args = Array.prototype.slice.call(arguments);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.setDebug = function(debug)
+{
+	uesp.debug = debug;
+	
+	if (uesp.debug)
+		uesp.logDebug = uesp.priv_logDebug;
+	else
+		uesp.logDebug = function() { };
+}
+
+uesp.setDebug(uesp.DEFAULT_DEBUG);
+
+
+uesp.priv_log = function () {
+	var args = Array.prototype.slice.call(arguments);
+	var logLevel = args[0];
+	
+	if (logLevel >= uesp.LOG_LEVEL_ALL && logLevel <= uesp.LOG_LEVEL_NONE)
+		args.splice(0, 1);
+	else
+		logLevel = uesp.logLevel;
+	
+	if (logLevel >= uesp.logLevel && window.console) 
+	{
+		if (args.length == 1)
+			console.log(args[0]);
+		else
+			console.log(args);
+	}
+	
+}
+
+
+uesp.log = function ()
+{
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(uesp.LOG_LEVEL_ALL);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.priv_logCritical = function ()
+{
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(uesp.LOG_LEVEL_CRITICAL);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.priv_logError = function ()
+{
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(uesp.LOG_LEVEL_ERROR);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.priv_logWarning = function ()
+{
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(uesp.LOG_LEVEL_WARNING);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.priv_logInfo = function ()
+{
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(uesp.LOG_LEVEL_INFO);
+	uesp.priv_log.apply(this, args);
+}
+
+
+uesp.setLogLevel = function(logLevel)
+{
+	uesp.logLevel = logLevel;
+	
+	uesp.logCritical = function() { };
+	uesp.logError    = function() { };
+	uesp.logWarning  = function() { };
+	uesp.logInfo     = function() { };
+	
+	if (uesp.logLevel <= uesp.LOG_LEVEL_CRITICAL) uesp.logCritical = uesp.priv_logCritical;
+	if (uesp.logLevel <= uesp.LOG_LEVEL_ERROR   ) uesp.logError    = uesp.priv_logError;
+	if (uesp.logLevel <= uesp.LOG_LEVEL_WARNING ) uesp.logWarning  = uesp.priv_logWarning;
+	if (uesp.logLevel <= uesp.LOG_LEVEL_INFO    ) uesp.logInfo     = uesp.priv_logInfo;	
+}
+
+uesp.setLogLevel(uesp.DEFAULT_LOG_LEVEL);
