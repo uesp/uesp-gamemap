599fd9757dfbe2905f2db24de964596ad2af071d
Dave Humphrey <dave@uesp.net>
2014-01-24 09:14:53 -0500

    add location class

---
+++ location.js
@@ -0,0 +1,40 @@
+/*
+ * location.js -- Created by Dave Humphrey (dave@uesp.net) on 23 Jan 2014
+ * 		Released under the GPL v2
+ *
+ */
+
+
+uesp.gamemap.LOCTYPE_NONE  = 0;
+uesp.gamemap.LOCTYPE_POINT = 1;
+uesp.gamemap.LOCTYPE_PATH  = 2;
+uesp.gamemap.LOCTYPE_AREA  = 3;
+
+
+uesp.gamemap.Location = function()
+{
+	this.id = -1;
+	this.worldId = -1;
+	this.revisionId = -1;
+	this.destinationId = -1;
+	this.x = 0;
+	this.y = 0;
+	this.width = 0;
+	this.height = 0;
+	this.name = "";
+	this.description = "";
+	this.wikiPage = "";
+	this.locType = 0;
+	this.displayLevel = 0;
+	this.visible = false;
+	
+	this.displayData = {};
+	//this.iconFile = 0;
+	//this.editorId = 0;
+	//this.formId = 0;
+	//this.points = [];
+	//this.fontSize = 8;
+	//this.fontColor = '#ff9999';
+	//this.customStyle = '';
+	//this.customClass = '';
+}
