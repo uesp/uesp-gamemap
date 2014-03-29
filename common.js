7fc8c1efef0180ecd438d1cd83334315f31ef3bb
Dave Humphrey <uespnet@gmail.com>
2014-03-29 16:51:29 -0400

    auto fill in values for common elements

--- common.js
+++ common.js
@@ -34,6 +34,12 @@ uesp.gamemap.isNullorUndefined = function (variable)
 }
 
 
+uesp.endsWith = function (str, suffix)
+{
+	return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+
 uesp.gamemap.createMergedObject = function (obj1, obj2)
 {
 	var obj3 = {};
