ce3f9e2af69031706ce2ed0e519270d42443ab5b
Thal-J <irgmisg4nxc9@beconfidential.com>
2022-08-16 20:09:09 +0100

    feat: add hiding clear search button

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+ * common.js -- Created by Thal-J (thal-j@uesp.net) on 16th Aug 2022
+ * 		Released under the GPL v2
+ * 		Contains main code for the Gamemap system.
+ */
+
 window.onload = function() {
 
     // bind views from DOM
@@ -6,22 +12,29 @@ window.onload = function() {
 
     // setup event listeners
     btn_clear_search.addEventListener("click", clearSearch);
-    searchbox.addEventListener("input", updateSearch(searchbox.value));
-
+    searchbox.addEventListener("input", function(){ updateSearch(searchbox.value); });
 }
+
+/*======================================
+                Search
+======================================*/
 
 function clearSearch() {
 
-    console.log("teafa");
-
     searchbox.value = "";
-
+    console.log("cleared search.");
+    btn_clear_search.style.visibility = 'hidden';
 
 }
 
 function updateSearch(query) {
+    console.log("search query: " + query);
 
-    console.log(query);
-
+    // control clear search button visibility
+    if (query.length > 0) {
+        btn_clear_search.style.visibility = 'visible';
+    } else {
+        btn_clear_search.style.visibility = 'hidden';
+    }
 }
   
