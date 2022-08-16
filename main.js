37ba95584c1d95831b6119fb3e8a530824206ae1
Thal-J <irgmisg4nxc9@beconfidential.com>
2022-08-16 21:04:16 +0100

    refactor: add banishdefault and hidedefault selectors

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * main.js -- Created by Thal-J (thal-j@uesp.net) on 16th Aug 2022
- * 		Released under the GPL v2
- * 		Contains main code for the Gamemap system.
+ * 	Released under the GPL v2
+ * 	Contains main code for the Gamemap system.
  */
 
 window.onload = function() {
@@ -15,6 +15,7 @@ window.onload = function() {
     searchbox.addEventListener("input", function(){ updateSearch(searchbox.value); });
 
     // TODO: add event listeners to up/down and enter here as well, to control the search list selection
+    // BEHAVIOUR: "Enter" selects the first item in the list, arrow keys move the selection, mouse takes priority
 
     // hijack ctrl + F to redirect to custom search
     $(window).keydown(function(event){
@@ -26,9 +27,9 @@ window.onload = function() {
     
 }
 
-/*======================================
-                Search
-======================================*/
+/*================================================
+	                  Search
+================================================*/
 
 function focusSearch() {
     searchbox.focus();
@@ -43,7 +44,7 @@ function clearSearch() {
 function updateSearch(query) {
     console.log("search query: " + query);
 
-    // control clear search button visibility
+    // toggle clear button visibility
     if (query.length > 0) {
         btn_clear_search.style.visibility = 'visible';
     } else {
