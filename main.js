64bd0845ee786111c6e295de41c99b9a58804d89
Thal-J <irgmisg4nxc9@beconfidential.com>
2022-08-16 20:24:28 +0100

    feat: add ctrl+f shortcut to search

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
- * common.js -- Created by Thal-J (thal-j@uesp.net) on 16th Aug 2022
+ * main.js -- Created by Thal-J (thal-j@uesp.net) on 16th Aug 2022
  * 		Released under the GPL v2
  * 		Contains main code for the Gamemap system.
  */
@@ -13,18 +13,31 @@ window.onload = function() {
     // setup event listeners
     btn_clear_search.addEventListener("click", clearSearch);
     searchbox.addEventListener("input", function(){ updateSearch(searchbox.value); });
+
+    // TODO: add event listeners to up/down and enter here as well, to control the search list selection
+
+    // hijack ctrl + F to redirect to custom search
+    $(window).keydown(function(event){
+        if ((event.ctrlKey || event.metaKey) && event.keyCode === 70) {
+            event.preventDefault();
+            focusSearch();
+        }
+    });
+    
 }
 
 /*======================================
                 Search
 ======================================*/
 
-function clearSearch() {
+function focusSearch() {
+    searchbox.focus();
+}
 
+function clearSearch() {
     searchbox.value = "";
     console.log("cleared search.");
     btn_clear_search.style.visibility = 'hidden';
-
 }
 
 function updateSearch(query) {
