950b34eb8e5e28d312e1be2aae663683495a2ad6
Thal-J <irgmisg4nxc9@beconfidential.com>
2022-08-26 13:23:03 +0100

    refactor: move gamemap code out of index.html

--- mapoptions.js
+++ mapoptions.js
@@ -45,14 +45,12 @@ uesp.gamemap.MapOptions = function(mapOptions)
 	this.initialGamePosY = 0;
 	this.initialZoom     = 10;
 	
-	this.mapLinkElement = '#gmMapLink';
-	
 	this.homeWorldId = 668;
 	
-		// Map iconTypes values to strings
+	// Map iconTypes values to strings
 	this.iconTypeMap = null;
 	
-		// Temporary values for the initial ESO maps
+	// Temporary values for the initial ESO maps
 	this.initialGamePosX = 500000;
 	this.initialGamePosY = 600000;
 	
