77fb37d67d672cf8d8a1d52a9cd679e42d5ce249
Thal-J <irgmisg4nxc9@beconfidential.com>
2023-03-06 21:17:16 +0000

    design: edit pane entrance animation

--- src/components/EditPane.svelte
+++ src/components/EditPane.svelte
@@ -5,18 +5,39 @@
 ### Author(s)
 - Thal-J <thal-j@uesp.net> (21st Feb, 2023) -->
 
-
-
 <script>
 
+    // import svelte core stuff
     import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+    import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
+    const progress = tweened(0, {
+		duration: 400,
+		easing: cubicOut,
+	});
+
+    let editPanel;
+    const PANEL_WIDTH = 500;
+    const ANIMATION_DURATION = 350;
+
+	onMount(async() => {
+
+
+        progress.set(1);
+
+        print($progress);
+
+    });
 
 </script>
 
 
 <markup>
-    <aside id="edit-panel" in:fade>
+    <aside id="edit-panel" bind:this={editPanel} in:fade={{duration: ANIMATION_DURATION}} style="width: {$progress * PANEL_WIDTH}px;">
+
+        <b>{$progress}</b>
 
 
     </aside>
@@ -26,9 +47,10 @@
 <style>
     #edit-panel {
         background-color: var(--surface);
-        width: var(--side_panel_width);
+        width: 0px;
         height: 100%;
         z-index: 100;
         position: relative;
+        box-shadow: 0px 1.5px 4px 4px var(--shadow);
     }
 </style>
