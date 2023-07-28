bda49b72d42114101030dde511c834ec9dabf966
Dave Humphrey <dave@uesp.net>
2023-07-28 15:18:35 -0400

    merge change for dev

--- gamemap.php
+++ gamemap.php
@@ -13,7 +13,7 @@
 
 	// Return local secrets file if localhost or hosted version if on uesp servers
 if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', "localhost"))) {
-	require 'gamemap.secrets';
+	require '../../../secrets/gamemap.secrets';
 } else {
 	require '/home/uesp/secrets/gamemap.secrets';
 }
